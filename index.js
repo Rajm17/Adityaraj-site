8dcd423dcee3d4f76dc8fc40fd5b88863dbea0f8
raj <107979020+Rajm17@users.noreply.github.com>
2024-06-30 23:12:40 +0530

    implement forms

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+function loaded() {
+  document.getElementById('contact-form').addEventListener("submit", handleFormSubmit);
+  document.getElementById('popup-contact-form').addEventListener("submit", handlePopupForm);
+  document.getElementById('popup-phone').addEventListener('change', phoneNumberValidation);
+  document.getElementById('phone').addEventListener('change', phoneNumberValidation);
+};
+
+document.addEventListener("DOMContentLoaded", loaded, false);
+
+// Handlers move-to-top and whatsapp chat function
 window.addEventListener('scroll', function () {
   const scrollY = window.scrollY; // Get current scroll position
   const moveTopBtn = document.getElementById('move-top');
   const whatsappRedirectBtn = document.getElementById('whatsapp-redirect');
-  const threshold = 100; // Show button after scrolling more than 100px
+  const threshold = 100;
 
   if (scrollY > threshold) {
     moveTopBtn.classList.add('visible');
@@ -14,63 +24,17 @@ window.addEventListener('scroll', function () {
 });
 
 
+// Handles Poppers
 const popperElements = document.querySelectorAll(".poppers");
 
 for (const popperElement of popperElements) {
   popperElement.addEventListener("click", openPopupForm);
 }
 
-
-// Main contact form
-const form = document.getElementById("contact-form");
-const phoneInput = document.getElementById("phone");
-const phoneError = document.getElementById("phone-error");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const phoneNumber = phoneInput.value;
-  const indianPhoneRegex = /^[6-789]\d{9}$/;
-  if (!indianPhoneRegex.test(phoneNumber)) {
-    phoneError.style.visibility = "visible";
-    phoneError.textContent = "Invalid phone number. Please enter valid 10-digit number";
-    phoneInput.classList.add("error");
-    return;
-  }
-
-  phoneError.style.visibility = "hidden";
-  phoneInput.classList.remove("error");
-
-  console.log("Form submitted! Name:", document.getElementById("name").value, ", Phone:", phoneNumber);
-  form.reset();
-});
-
-
-// Popup contact form:
-const popupForm = document.getElementById("popup-contact-form");
-const popupPhoneInput = document.getElementById("popup-phone");
-const popupPhoneError = document.getElementById("popup-phone-error");
-
-popupForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const phoneNumber = popupPhoneInput.value;
-  const indianPhoneRegex = /^[6-789]\d{9}$/;
-  if (!indianPhoneRegex.test(phoneNumber)) {
-    popupPhoneError.style.visibility = "visible";
-    popupPhoneError.textContent = "Invalid phone number. Please enter valid 10-digit number";
-    return;
-  }
-
-  console.log("Popup Form submitted! Name:", document.getElementById("popup-name").value, ", Phone:", phoneNumber);
-  closePopupForm();
-});
-
-document.getElementById('popup-overlay').addEventListener("onclick", closePopupForm());
-
 function openPopupForm() {
   document.getElementById(
     "popup-form-container"
   ).style.display = "block";
-
   document.getElementById(
     "popup-overlay"
   ).style.display = "block";
@@ -78,17 +42,85 @@ function openPopupForm() {
 
 function closePopupForm() {
   document.getElementById(
+    "popup-contact-form"
+  ).reset();
+  document.getElementById(
     "popup-form-container"
   ).style.display = "none";
-
   document.getElementById(
     "popup-overlay"
   ).style.display = "none";
+}
 
-  document.getElementById(
-    "popup-contact-form"
-  ).reset();
+function getFormData(form) {
+  var elements = form.elements;
+  var honeypot;
+  var formData = {};
+  formData.full_name = elements.full_name.value;
+  formData.email = elements.email.value;
+  formData.phoneNumber = elements.phone_number.value;
+  formData.honeypot = elements._protect.checked;
+  return { data: formData, honeypot: honeypot };
+}
 
-  const popupPhoneError = document.getElementById("popup-phone-error");
-  popupPhoneError.style.visibility = "hidden";
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var form = event.target;
+  var formData = getFormData(form);
+
+  if (formData == null || formData.honeypot) {
+    return;
+  }
+
+  var data = formData.data;
+
+  pauseButtonDuringRequest(form, true);
+  var url = "https://script.google.com/macros/s/AKfycbx3Hkry71vVkiKa87beijZuVwJXscgT4_GEw6EqPt3u5F_OVzP7CJx7PWXd547xYWspkw/exec";
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      form.reset();
+      var thankYouMessage = form.querySelector(".thankyou_message");
+      if (thankYouMessage) {
+        thankYouMessage.style.display = "block";
+      }
+    }
+    pauseButtonDuringRequest(form, false);
+  };
+  var encoded = Object.keys(data).map(function (k) {
+    return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+  }).join('&');
+
+  try {
+    console.log(encoded);
+    xhr.send(encoded);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function handlePopupForm(event) {
+  var result = handleFormSubmit(event);
+  if (result == null)
+    closePopupForm();
+}
+
+function phoneNumberValidation(ev) {
+  const input = ev.target;
+  const indianPhoneRegex = /^[6-789]\d{9}$/;
+  if (!indianPhoneRegex.test(input.value)) {
+    input.setCustomValidity("Invalid phone number. Please enter valid 10-digit number");
+  } else {
+    input.setCustomValidity("");
+  }
+}
+
+function pauseButtonDuringRequest(form, isPause) {
+  var buttons = form.querySelectorAll("button");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = isPause;
+  }
 }
